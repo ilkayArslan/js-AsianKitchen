90e9dc5625d083b74d4233fd4e73c6446a715e5d
ilkayArslan <ilkayarslan1035@gmail.com>
2022-07-14 16:26:13 +0300

    onaylandi

---
+++ app.js
@@ -0,0 +1,194 @@
+const menu = [
+    {
+      id: 1,
+      title: "Tteokbokki",
+      category: "Korea",
+      price: 10.99,
+      img:
+        "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+      desc: `Spicy rice cakes, serving with fish cake.`,
+    },
+    {
+      id: 2,
+      title: "Chicken Ramen",
+      category: "Japan",
+      price: 7.99,
+      img:
+        "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+      desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+    },
+    {
+      id: 3,
+      title: "Bibimbap",
+      category: "Korea",
+      price: 8.99,
+      img:
+        "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+      desc: `Boiling vegetables, serving with special hot sauce`,
+    },
+    {
+      id: 4,
+      title: "Dan Dan Mian",
+      category: "China",
+      price: 5.99,
+      img:
+        "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+      desc: `Dan dan noodle, serving with green onion `,
+    },
+    {
+      id: 5,
+      title: "Yangzhou Fried Rice",
+      category: "China",
+      price: 12.99,
+      img:
+        "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+      desc: `Yangzhou style fried rice, serving with bean and pickles `,
+    },
+    {
+      id: 6,
+      title: "Onigiri",
+      category: "Japan",
+      price: 9.99,
+      img:
+        "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+      desc: `Rice Sandwich, serving with soy sauce`,
+    },
+    {
+      id: 7,
+      title: "Jajangmyeon",
+      category: "Korea",
+      price: 15.99,
+      img:
+        "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+      desc: `Black bean sauce noodle, serving with green onion `,
+    },
+    {
+      id: 8,
+      title: "Ma Yi Shang Shu",
+      category: "China",
+      price: 12.99,
+      img:
+        "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+      desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+    },
+    {
+      id: 9,
+      title: "Doroyaki",
+      category: "Japan",
+      price: 3.99,
+      img:
+        "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+      desc: `Red bean paste dessert, serving with honey.`,
+    },
+  ];
+
+  let butonArea = document.querySelector(".btn-container");
+  butonArea.innerHTML =`<button class="btn btn-outline-dark btn-item">All</button>
+  <button class="btn btn-outline-dark btn-item">Korea</button>
+  <button class="btn btn-outline-dark btn-item">Japan</button>
+  <button class="btn btn-outline-dark btn-item">China</button>`;
+  /* Açılan butonların yerleştirilmesi yapılmıştır.*/
+  let sectionArea = document.querySelector(".section-center");
+  menu.forEach((eleman)=>{
+    sectionArea.innerHTML +=    `<div class="menu-items col-lg-6 col-sm-12">
+            <img src='${eleman.img}' class="photo" alt="${eleman.title}">
+            <div class="menu-info">
+                <div class = "menu-title">
+                    <h4>${eleman.title}</h4>
+                    <h4 class = "price">${eleman.price}</h4>
+                </div>
+                <div class = "menu-text">
+                    ${eleman.desc}
+                </div>
+            </div>
+        </div>`
+  });//açılıştaki menunun oluşturulması...
+   let koreanMenu = [];
+   let japanMenu = [];
+   let chinaMenu = [];
+   //oluşturulan menulerle butonlara event atamasının yapılması
+  butonArea.addEventListener("click",function(event){
+    let categoryName = event.target.innerHTML;
+    console.log( categoryName);
+    //ilk koşulumuz olan Korea butonun oluşturulması ve korea yemeklerinin filtrelenmesi
+    if(categoryName == "Korea"){
+       koreanMenu = menu.filter(food=> food.category == "Korea") ;     
+       sectionArea.innerHTML ="";
+      koreanMenu.forEach(koreanFood=>{
+        sectionArea.innerHTML +=    `<div class="menu-items col-lg-6 col-sm-12">
+            <img src='${koreanFood.img}' class="photo" alt="${koreanFood.title}">
+            <div class="menu-info">
+                <div class = "menu-title">
+                    <h4>${koreanFood.title}</h4>
+                    <h4 class = "price">${koreanFood.price}</h4>
+                </div>
+                <div class = "menu-text">
+                    ${koreanFood.desc}
+                </div>
+            </div>
+        </div>`
+      }) 
+    }
+    //İkinci Koşulumuz olan Japan food ile filtrelemenin yapılması
+     else if(categoryName == "Japan"){
+      japanMenu = menu.filter(food=> food.category == "Japan") ;
+       sectionArea.innerHTML ="";
+      japanMenu.forEach(eleman=>{
+        sectionArea.innerHTML +=    `<div class="menu-items col-lg-6 col-sm-12">
+            <img src='${eleman.img}' class="photo" alt="${eleman.title}">
+            <div class="menu-info">
+                <div class = "menu-title">
+                    <h4>${eleman.title}</h4>
+                    <h4 class = "price">${eleman.price}</h4>
+                </div>
+                <div class = "menu-text">
+                    ${eleman.desc}
+                </div>
+            </div>
+        </div>`
+      }) 
+    }
+    //China food ile üçüncü koşulun yapılması
+    else if(categoryName == "China"){
+      chinaMenu = menu.filter(food=> food.category == "China") 
+       sectionArea.innerHTML ="";
+      chinaMenu.forEach(eleman=>{
+        sectionArea.innerHTML +=    `<div class="menu-items col-lg-6 col-sm-12">
+            <img src='${eleman.img}' class="photo" alt="${eleman.title}">
+            <div class="menu-info">
+                <div class = "menu-title">
+                    <h4>${eleman.title}</h4>
+                    <h4 class = "price">${eleman.price}</h4>
+                </div>
+                <div class = "menu-text">
+                    ${eleman.desc}
+                </div>
+            </div>
+        </div>`
+      }) 
+      //enson olarak All butonunun sağlamasının yapılması
+    }else if(categoryName =="All"){
+      sectionArea.innerHTML ="";
+      menu.forEach(eleman=>{
+        sectionArea.innerHTML +=    `<div class="menu-items col-lg-6 col-sm-12">
+            <img src='${eleman.img}' class="photo" alt="${eleman.title}">
+            <div class="menu-info">
+                <div class = "menu-title">
+                    <h4>${eleman.title}</h4>
+                    <h4 class = "price">${eleman.price}</h4>
+                </div>
+                <div class = "menu-text">
+                    ${eleman.desc}
+                </div>
+            </div>
+        </div>`
+      })
+    }
+    
+    
+
+
+    
+  })
+  
+ 
